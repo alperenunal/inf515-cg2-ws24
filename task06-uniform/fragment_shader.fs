93e1576cf66424e8c7e544d6ddaa5d99b590eaff
Alp Eren Ünal <alpernunl@gmail.com>
2024-10-20 17:52:49 +0300

    add: solution of blatt-3 task-4

--- task06-uniform/fragment_shader.fs
+++ task06-uniform/fragment_shader.fs
@@ -5,6 +5,34 @@
 #version 330 core
 out vec4 frag_color;
 in vec3 our_color;
-void main() {
-  frag_color = vec4(our_color.xyz, 1.0f);
+uniform int color_mode;
+void main(){
+  frag_color = vec4(0.0, 0.0, 0.0, 1.0);
+  int r = (color_mode&0x30) >> 4;
+  int g = (color_mode&0x0c) >> 2;
+  int b = (color_mode&0x03);
+  
+  if (r == 2) {
+    frag_color.x = our_color.y;
+  } else if (r == 3) {
+    frag_color.x = our_color.z;
+  } else {
+    frag_color.x = our_color.x;
+  }
+  
+  if (g == 1){
+    frag_color.y = our_color.x;
+  } else if (g == 3) {
+    frag_color.y = our_color.z;
+  } else {
+    frag_color.y = our_color.y;
+  }
+  
+  if (b == 1) {
+    frag_color.z = our_color.x;
+  } else if (b == 2) {
+    frag_color.z = our_color.y;
+  } else {
+    frag_color.z = our_color.z;
+  }
 }
--- task06-uniform/vertex_shader.vs
+++ task06-uniform/vertex_shader.vs
@@ -6,7 +6,8 @@
 layout (location = 0) in vec3 pos;
 layout (location = 1) in vec3 color;
 out vec3 our_color;
+uniform float scale;
 void main(){
-  gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
+  gl_Position = vec4(pos.x * scale, pos.y * scale, pos.z * scale, 1.0);
   our_color = color;
 }
