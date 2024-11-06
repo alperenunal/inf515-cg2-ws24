923867c4c252b25a847a8005e993247a497102d0
Alp Eren Ünal <alpernunl@gmail.com>
2024-11-06 22:32:40 +0300

    add: solution to blatt-5 task-2

--- task09-uv_coordinates/fragment_shader.fs
+++ task09-uv_coordinates/fragment_shader.fs
@@ -2,14 +2,16 @@
 out vec4    frag_color;
 in vec3     out_color;
 // ToDo step 10: receive texture coordiantes from vertex shader
+in vec2 tex_coord;
 uniform int render_mode;
 // ToDo step 12: provide a sampler / texture unit to read the texture
+uniform sampler2D tex;
 
 void
 main() {
   // --- read texture color
   // ToDo step 13: replace code to read color from texture
-  vec4 tex_col = vec4(1.0);
+  vec4 tex_col = texture(tex, tex_coord);
 
   // --- set color based on mode
   const int modes = 2;
